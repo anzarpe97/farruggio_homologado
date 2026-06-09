bd7cb05c6db92f72d6acba0fddef432281a53abe
Anderson Salazar <anzarpe10@gmail.com>
2026-06-09 09:36:41 -0400

    Arreglos

--- garazd_product_label/models/res_config_settings.py
+++ garazd_product_label/models/res_config_settings.py
@@ -4,4 +4,7 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    replace_standard_wizard = fields.Boolean(config_parameter='garazd_product_label.replace_standard_wizard')  # flake8: noqa: E501
+    replace_standard_wizard = fields.Boolean(
+        string='Print with the alternative wizard',
+        config_parameter='garazd_product_label.replace_standard_wizard',
+    )
